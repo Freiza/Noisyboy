4021c0f75ef184251f349401f25124d4b5aad7a9
freiza <fraserbuddy@gmail.com>
2016-10-24 04:13:35 +0530

    Gradle libgdx
    Gradle builds libgdx project

---
+++ desktop/src/freiza/experiment/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package freiza.experiment.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import freiza.experiment.Experimental;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Experimental(), config);
+	}
+}
